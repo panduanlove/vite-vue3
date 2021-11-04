788516b041ad4894bf7ee8896fa1d4e7e69a483b
panfengshan <panfengshan@haodf.com>
2021-11-04 16:24:10 +0800

    初始化项目

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import HelloWorld from '../components/HelloWorld.vue';
+</script>
+
+<template>
+  <div class="home" @click="toVuex">to vuex</div>
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'Vuex',
+  setup () {
+    const count = ref < number > (0);
+    const increment = () => {
+      count.value += 1;
+    };
+    return { count, increment };
+  },
+  methods:{
+    toVuex(){
+      console.log(this.$router.push('/vuex'));
+    }
+  }
+
+});
+</script>
+<style>
+.home{
+  color:red;
+}
+</style>
---
+++ src/views/Vuex.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="vuex" @click="toTest">toTest</div>
+  <div>{{count}}</div>
+  <div @click="increment">count++</div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  name: 'Vuex',
+  setup () {
+    const count = ref < number > (10);
+    const increment = () => {
+      count.value += 1;
+    };
+    return { count, increment };
+  },
+  methods:{
+    toTest(){
+      this.$router.push('/test');
+    }
+  }
+
+});
+</script>
+<style>
+.vuex{
+  color:green;
+}
+</style>
